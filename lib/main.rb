addf5dd53785d8c19cc40d1cdbc8b74ccb5438d6
Fred Mitchell <lordalveric@yahoo.com>
2014-06-07 10:34:12 +0200

    building the main interface

--- lib/main.rb
+++ lib/main.rb
@@ -1,1 +1,54 @@
 # Sinatra app to test recom
+require 'sinatra/base'
+require 'sinatra/assetpack'
+require 'sinatra-websocket'
+require 'barista'
+require 'sass'
+require 'haml'
+require 'logger'
+require_relative 'recommendation'
+
+module Main
+  class Recon < Sinatra::Base
+    include Recommendation
+
+    set :root, File.expand_path('..', File.dirname(__FILE__))
+    set :logging, true
+    set :server, 'thin'
+    set :sockets, []
+
+    register Barista::Integration::Sinatra
+    register Sinatra::AssetPack
+
+    configure do
+      set port: 3912
+      set static: true
+      use Rack::CommonLogger, $log = ::Logger.new(::File.new('log/recon.log', 'a+'))
+      $log.debug "Started Recon at #{Time.now}"
+      Barista.add_preamble do |location|
+        %{
+          /* DO NOT MODIFY -- compiled from #{location}
+           */
+        }
+      end
+    end
+
+    assets do
+      serve '/js',      from: 'app/js'           # Default
+      serve '/css',     from: 'app/css'          # Default
+      serve '/images',  from: 'app/images'       # Default
+    end
+
+    get '/' do
+      haml :index
+    end
+
+    post '/recom' do
+      @items = recommend(@user = params[:user])
+      haml :recommend
+    end
+
+  end
+end
+
+Main::Recon.run!
--- lib/recommendation.rb
+++ lib/recommendation.rb
@@ -1,5 +1,7 @@
 # Recommendation Algorithm
 
 module Recommendation
-
+  def recommend(user)
+    [11,22,33,44,55]
+  end
 end
